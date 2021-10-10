b73e50c9aaefb26f8349f2755edc8647d118c9e9
go-laoji <us.hipeace86@gmail.com>
2021-10-10 23:44:06 +0800

    客户管理-客户信息获取

--- external_contact_test.go
+++ external_contact_test.go
@@ -4,6 +4,10 @@ import (
 	"testing"
 )
 
+//
+// 涉及到外部联系人的id不好获取，单元测试不完整
+//
+
 func TestWorkChat_ExternalContactGetFollowUserList(t *testing.T) {
 	resp := testWorkChat.ExternalContactGetFollowUserList()
 	if resp.ErrCode != 0 {
@@ -11,4 +15,20 @@ func TestWorkChat_ExternalContactGetFollowUserList(t *testing.T) {
 		return
 	}
 	t.Log(resp)
+}
+
+func TestWorkChat_ExternalContactList(t *testing.T) {
+
+}
+
+func TestWorkChat_ExternalContactGet(t *testing.T) {
+
+}
+
+func TestWorkChat_ExternalContactBatchGetByUser(t *testing.T) {
+
+}
+
+func TestWorkChat_ExternalContactRemark(t *testing.T) {
+
 }
