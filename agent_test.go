b9b3a0789294c3da05430f26f313c4ae84dfeb27
go-laoji <us.hipeace86@gmail.com>
2021-10-18 23:50:59 +0800

    应用管理

---
+++ agent_test.go
@@ -0,0 +1,28 @@
+package workchatapp
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWorkChat_AgentGet(t *testing.T) {
+	resp := testWorkChat.AgentGet()
+	if resp.ErrCode != 0 {
+		t.Error(resp.ErrorMsg)
+		return
+	}
+	t.Log(resp)
+}
+
+func TestWorkChat_AgentList(t *testing.T) {
+	resp := testWorkChat.AgentList()
+	if resp.ErrCode != 0 {
+		t.Error(resp.ErrorMsg)
+		return
+	}
+	if fmt.Sprintf("%v", resp.AgentList[0].AgentId) != testConfig.AppId {
+		t.Error("Fail")
+		return
+	}
+	t.Log(resp)
+}
